5ef59e7323370fb6ab488fc051da7fefd67cc054
Louis-Adolphe MOUGNIN <mougnin@intechinfo.fr>
2015-12-12 15:44:50 +0100

    Modifs sur tableau de bord

--- vue/vue_gestion_relais.php
+++ vue/vue_gestion_relais.php
@@ -11,12 +11,12 @@
 
     <title>Tableau de bord OPENWORLD</title>
 
-    <link href="../bootstrap/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="../bootstrap/bower_components/metisMenu/dist/metisMenu.min.css" rel="stylesheet">
-    <link href="../bootstrap/dist/css/timeline.css" rel="stylesheet">
-    <link href="../bootstrap/dist/css/sb-admin-2.css" rel="stylesheet">
-    <link href="../bootstrap/bower_components/morrisjs/morris.css" rel="stylesheet">
-    <link href="../bootstrap/bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="./bootstrap/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="./bootstrap/bower_components/metisMenu/dist/metisMenu.min.css" rel="stylesheet">
+    <link href="./bootstrap/dist/css/timeline.css" rel="stylesheet">
+    <link href="./bootstrap/dist/css/sb-admin-2.css" rel="stylesheet">
+    <link href="./bootstrap/bower_components/morrisjs/morris.css" rel="stylesheet">
+    <link href="./bootstrap/bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
 </head>
 
@@ -151,21 +151,21 @@
             </ol>
 
     <!-- jQuery -->
-    <script src="../bootstrap/bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="./bootstrap/bower_components/jquery/dist/jquery.min.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
-    <script src="../bootstrap/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="./bootstrap/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
 
     <!-- Metis Menu Plugin JavaScript -->
-    <script src="../bootstrap/bower_components/metisMenu/dist/metisMenu.min.js"></script>
+    <script src="./bootstrap/bower_components/metisMenu/dist/metisMenu.min.js"></script>
 
     <!-- Morris Charts JavaScript -->
-    <script src="../bootstrap/bower_components/raphael/raphael-min.js"></script>
-    <script src="../bootstrap/bower_components/morrisjs/morris.min.js"></script>
-    <script src="../bootstrap/js/morris-data.js"></script>
+    <script src="./bootstrap/bower_components/raphael/raphael-min.js"></script>
+    <script src="./bootstrap/bower_components/morrisjs/morris.min.js"></script>
+    <script src="./bootstrap/js/morris-data.js"></script>
 
     <!-- Custom Theme JavaScript -->
-    <script src="../bootstrap/dist/js/sb-admin-2.js"></script>
+    <script src="./bootstrap/dist/js/sb-admin-2.js"></script>
 
 </body>
 
