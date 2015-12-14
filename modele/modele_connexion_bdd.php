415ac2d3d34825e06bd9fe9b2dc99923ec3dfcba
Hassane Sangare <sangare@intechinfo.fr>
2015-12-14 10:13:41 +0100

    ejvnjen

--- modele/modele_connexion_bdd.php
+++ modele/modele_connexion_bdd.php
@@ -1,6 +1,6 @@
 <?php
 	try{
-		$bdd = new PDO("mysql:host=localhost;dbname=openworld;charset=utf8", "root", "", array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+		$bdd = new PDO("mysql:host=localhost;dbname=openworld;charset=utf8", "root", "openworld", array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 	}catch(Exception $e){
 		die("ERREUR : ".$e->getMessage());
 	}
