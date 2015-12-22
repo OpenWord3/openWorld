6e94778c0815f684ee2b292bf86593986363ed39
Hassane Sangare <sangare@intechinfo.fr>
2015-12-22 03:36:43 +0100

    djbvd;uovbdsuvb

--- controleur/controleur_gestion_blog.php
+++ controleur/controleur_gestion_blog.php
@@ -9,31 +9,25 @@
 
 		switch($_GET["action"]){
 			case "acceder_blog":
-				$verif_blog = blog($id);
-				if($verif_blog == ""){
-					$alerte = "Vous n'avez pas de blog actuellement.";
+				$verif_status_blog = status_blog($id);
+				if($verif_status_blog == "1"){
+					header("location:http://".$pseudo.".openworld.itinet.fr");
+				} else if($verif_status_blog == "2") {
+					$alerte = "Votre blog a été désactivé par l'administrateur.";
 					include("./vue/vue_gestion_blog.php");
 				} else {
-					$verif_status_blog = status_blog($id);
-					if($verif_status_blog == "1"){
-						header("location:http://".$pseudo.".openworld.itinet.fr");
-					} else if($verif_status_blog == "2") {
-						$alerte = "Votre blog a été désactivé par l'administrateur.";
-						include("./vue/vue_gestion_blog.php");
-					} else {
-						$alerte = "Votre blog n'est pas actif actuellement, vous devez d'abord l'activé.";
-						include("./vue/vue_gestion_blog.php");
-					}
-					
+					$alerte = "Votre blog n'est pas actif actuellement, vous devez d'abord l'activé.";
+					include("./vue/vue_gestion_blog.php");
 				}
+					
 				break;
 			case "activer_blog":
 				$verif_blog = blog($id);
 				if($verif_blog == ""){
 					$blog = $pseudo.".openworld.itinet.fr";
 					$status = "1";					
-					exec('sudo /var/script/add_vhost.sh '.$pseudo.' '.$mdp);
-					add_blog($id,$blog,$status); 
+					//exec('sudo /var/script/add_vhost.sh '.$pseudo.' '.$mdp);
+					//add_blog($id,$blog,$status); 
 					$alerte = "Votre blog vient d'être activé.";
 					include("./vue/vue_gestion_blog.php");
 				} else {
@@ -48,29 +42,28 @@
 						$alerte = "Votre blog vient d'être activé.";
 						$status = "1";
 						active_blog($id,$status);
-						exec('sudo /var/script/activation_vhost.sh '.$pseudo);
+						//exec('sudo /var/script/activation_vhost.sh '.$pseudo);
 						include("./vue/vue_gestion_blog.php");
 					}
 				}
 				break;
 			case "desactiver_blog":
-				$verif_blog = blog($id);
-				if($verif_blog == ""){
-					$alerte = "Vous n'avez pas de blog actuellement.";
+				
+				$verif_status_blog = status_blog($id);
+				if($verif_status_blog == "0"){
+					$alerte = "Votre blog est déjà désactivé.";
+					include("./vue/vue_gestion_blog.php");
+				}else if($verif_status_blog == "2"){
+					$alerte = "Votre blog a déjà été désactivé par l'administrateur.";
 					include("./vue/vue_gestion_blog.php");
 				} else {
-					$verif_status_blog = status_blog($id);
-					if($verif_status_blog == "0"){
-						$alerte = "Votre blog est déjà désactivé.";
-						include("./vue/vue_gestion_blog.php");
-					} else {
-						$alerte = "Votre blog vient d'être désactivé.";
-						$status = "0";
-						active_blog($id,$status);
-						exec('sudo /var/script/unactivation_vhost.sh '.$pseudo);
-						include("./vue/vue_gestion_blog.php");
-					}
+					$alerte = "Votre blog vient d'être désactivé.";
+					$status = "0";
+					active_blog($id,$status);
+					//exec('sudo /var/script/unactivation_vhost.sh '.$pseudo);
+					include("./vue/vue_gestion_blog.php");
 				}
+				
 				break;
 			case "supprimer_blog":
 				$verif_blog = blog($id);
@@ -80,8 +73,8 @@
 				} else {
 					$status = "0";
 					$alerte = "Votre blog vient d'être définitivement fermé.";
-					exec('sudo /var/script/remove_vhost.sh '.$pseudo);
-					del_blog($id,$status);
+					//exec('sudo /var/script/remove_vhost.sh '.$pseudo);
+					//del_blog($id,$status);
 					include("./vue/vue_gestion_blog.php");
 				}
 				break;
