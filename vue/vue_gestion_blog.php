a0e917e2c7ffef58a2ee8b034dfb9a7eeb6b17a0
Hassane Sangare <sangare@intechinfo.fr>
2015-12-22 05:43:32 +0100

    wegwvdvds

--- vue/vue_gestion_blog.php
+++ vue/vue_gestion_blog.php
@@ -113,19 +113,16 @@
                                 </div>
                             </div>
                         </div>
-<<<<<<< HEAD
+
                         <!--<a href="http://<?php echo $_SESSION["pseudo"];?>.openworld.itinet.fr">-->
                         <?php 
                             $id = id($_SESSION["pseudo"]);
                             $verif_blog = blog($id); 
                         ?>
                         <?php if(isset($_GET["action"])){ if($_GET["action"] == "acceder_blog"){echo $alerte;}} ?>
-                        <a href="<?php if($verif_blog == ''){ echo '#';}else{echo INDEX.'?index=vue_gestion_blog&action=acceder_blog';}?>">
+                        <a href="<?php if($verif_blog == ''){ echo '#';}else{echo INDEX.'?index=vue_gestion_blog&action=acceder_blog';}?>" onclick="window.open(this.href); return false;">
 
-=======
-                        <?php if(isset($_GET["action"])){ if($_GET["action"] == "acceder_blog"){echo $alerte;}} ?>
-                        <a href="<?php echo INDEX ?>?index=vue_gestion_blog&action=acceder_blog"  onclick="window.open(this.href); return false;">
->>>>>>> adolphe
+
                             <div class="panel-footer">
                                 <span class="pull-left">Accéder à son blog</span>
                                 <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
