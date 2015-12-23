e6d7f95d6a031f11a0ee1087d78ad8cf8390f779
Hassane Sangare <sangare@intechinfo.fr>
2015-12-23 01:42:08 +0100

    bdsjkvdshgvh

--- controleur/controleur_gestion_profil.php
+++ controleur/controleur_gestion_profil.php
@@ -9,11 +9,12 @@
 		//exec('sudo /var/script/remove_vhost.sh '.$pseudo);
 		//exec('sudo /var/script/del_mail_account.sh '.$pseudo);
 		$domain = domain_user($id);
+		var_dump($domain);
 
-		foreach($domain as $nom){			
+		/*foreach($domain as $nom){			
 			echo $nom;
 			//exec('sudo /var/script/del-relais.sh '.$nom);
-		}		
+		}*/		
 		
 		//del_relais2($id);
 		//logout($pseudo);
