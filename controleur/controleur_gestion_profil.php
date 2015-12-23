48f3db0bb3e4f2d33d5a0dd4db95cf624d468867
Hassane Sangare <sangare@intechinfo.fr>
2015-12-23 01:58:44 +0100

    ljbsdihdbsvy

--- controleur/controleur_gestion_profil.php
+++ controleur/controleur_gestion_profil.php
@@ -6,20 +6,20 @@
 	$id = id($pseudo);
 
 	if(isset($_GET["desinscrire"])){
-		//exec('sudo /var/script/remove_vhost.sh '.$pseudo);
-		//exec('sudo /var/script/del_mail_account.sh '.$pseudo);
+		exec('sudo /var/script/remove_vhost.sh '.$pseudo);
+		exec('sudo /var/script/del_mail_account.sh '.$pseudo);
 		$domain = domain_user($id);
-		//var_dump($domain);
+		
 
 		foreach($domain as $nom){			
-			echo $nom["nom_domain"];
-			//exec('sudo /var/script/del-relais.sh '.$nom);
+			
+			exec('sudo /var/script/del-relais.sh '.$nom["nom_domain"];);
 		}		
 		
-		//del_relais2($id);
-		//logout($pseudo);
-		include("./vue/vue_gestion_profil.php");
-		//header("location:index.php");
+		del_relais2($id);
+		logout($pseudo);
+		
+		header("location:index.php");
 	} else if (isset($_POST["modifier"])){
 		$nom = $_POST["nom"];
 		$prenom = $_POST["prenom"];
