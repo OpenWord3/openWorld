e85fcd9256508f45e1ce678a909728bbaf52f383
steephen93 <ilangovane@intechinfo.fr>
2016-01-27 14:54:06 +0100

    13:54

--- vue/vue_accueil.php
+++ vue/vue_accueil.php
@@ -104,7 +104,7 @@
             	foreach ($liste_star as $result) {
 					$pseudo = $result['pseudo'];
             ?>
-	            <div class="col-lg-3 col-md-4 col-xs-6 thumb taille">
+	            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
 	                <a href="<?php echo "http://",$pseudo,".openworld.itinet.fr"; ?>" class="thumbnail gallery-box" target="_blank" >
 	                    <img src="<?php echo $img[$pseudo]; ?>" class="img-responsive taille" alt="Image 1">
 	                    <div class="gallery-box-caption">
