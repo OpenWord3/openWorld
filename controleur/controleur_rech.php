cab90bb2d80b93d665af6e3f846e782317d51512
steephen93 <ilangovane@intechinfo.fr>
2016-01-27 17:07:11 +0100

    13:54

--- controleur/controleur_rech.php
+++ controleur/controleur_rech.php
@@ -24,7 +24,7 @@ $res_rech = $_POST['res_rech'];
 				$post_date = explode(" ",$res->post_date);
 				$post_content = substr($res->post_content, 0, 200);
 				$post_content = str_split($post_content, 100);
-				$rech[] = "<ol><h5><a href=" . $res->guid . " target=_blank><i class='fa fa-compass'></i> " . $res->post_title . "</a></h5></n><h4 style='color:green;'>" . $res->guid . "</h4> <i class='fa fa-caret-down'></i><br>" . $post_date[0] . "<span>" . htmlentities($post_content[0]) . "<br>" . htmlentities($post_content[1]) . "..." . "</span></ol>";
+				$rech[] = "<ol><h4><a href=" . $res->guid . " target=_blank><i class='fa fa-compass'></i> " . $res->post_title . "</a></h4></n><h5 style='color:green;'>" . $res->guid . "</h5> <i class='fa fa-caret-down'></i><br>" . $post_date[0] . "<span>" . htmlentities($post_content[0]) . "<br>" . htmlentities($post_content[1]) . "..." . "</span></ol>";
 			}			
 		}	
 	}
