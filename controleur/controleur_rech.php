42db0082d94d4475b50f68f966cdf868d733230f
steephen93 <ilangovane@intechinfo.fr>
2016-01-27 16:42:30 +0100

    13:54

--- controleur/controleur_rech.php
+++ controleur/controleur_rech.php
@@ -22,7 +22,8 @@ $res_rech = $_POST['res_rech'];
 			$rech_compteur++;	
 			foreach($recherche as $res){
 				$post_date = explode(" ",$res->post_date);
-				$rech[] = "<ol><h4><a href=" . $res->guid . " target=_blank><i class='fa fa-compass'></i> " . $res->post_title . "</a></h4></n>" . $res->guid . " <i class='fa fa-caret-down'></i><br>" . $post_date[0] . "<span>" . htmlentities($res->post_content) . "</span></ol>";
+				$post_content = substr($res->post_content, 0, 20);
+				$rech[] = "<ol><h4><a href=" . $res->guid . " target=_blank><i class='fa fa-compass'></i> " . $res->post_title . "</a></h4></n>" . $res->guid . " <i class='fa fa-caret-down'></i><br>" . $post_date[0] . "<span>" . htmlentities($post_content) . "</span></ol>";
 			}			
 		}	
 	}
