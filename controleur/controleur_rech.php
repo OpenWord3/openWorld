09aa0eedf51eb02d12c7aec71232c14d6a19ffa1
steephen93 <ilangovane@intechinfo.fr>
2016-01-28 12:04:19 +0100

    12

--- controleur/controleur_rech.php
+++ controleur/controleur_rech.php
@@ -24,8 +24,8 @@ $res_rech = $_POST['res_rech'];
 				$post_date = explode(" ",$res->post_date);
 				$post_content = substr($res->post_content, 0, 200);
 				$post_content = str_replace(chr(34),"'",$post_content);
-				$post_content = str_split($post_content, 100);
-				$rech[] = "<ol><h4><a href=" . $res->guid . " target=_blank><i class='fa fa-compass'></i> " . $res->post_title . "</a></h4></n><h5 style='color:green;'>" . $res->guid . " <i class='fa fa-caret-down'></i></h5></n>" . $post_date[0] . "  <label>" . strip_tags($post_content[0]) . "</label><br><label>" . strip_tags($post_content[1]) . "..." . "</label></ol>";
+				//$post_content = str_split($post_content, 100);
+				$rech[] = "<ol><h4><a href=" . $res->guid . " target=_blank><i class='fa fa-compass'></i> " . $res->post_title . "</a></h4></n><h5 style='color:green;'>" . $res->guid . " <i class='fa fa-caret-down'></i></h5></n>" . $post_date[0] . "  <label>" . strip_tags($post_content) . "</label><br><label>" . "..." . "</label></ol>";
 			}			
 		}	
 	}
